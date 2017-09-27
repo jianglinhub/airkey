080206a37e4cb526a870a7e0118bff0582981041
jianglin <jianglin@jianglindeMacBook-Pro.local>
2017-09-27 15:51:28 +0800

    清理垃圾文件

--- client/route.js
+++ client/route.js
@@ -2,13 +2,8 @@
  * Created by sky on 2017/5/9.
  */
 import KeyControl from './modules/pages/KeyControl.vue';
-import QrCode from './modules/pages/qrCode';
 
 const routes = [
-    {
-      path: '/qrCode',
-      component: QrCode
-    },
     {
         path: '*',
         component: KeyControl
